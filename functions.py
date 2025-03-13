c2d753244866a28b73868b069e8bc568c7a3280c
farhanfahmid <fahmidfarhan274@gmail.com>
2025-03-13 17:42:24 +0600

    Add zip extraction feature

--- functions.py
+++ functions.py
@@ -7,3 +7,8 @@ def make_archive(filepaths, destination):
         for filepath in filepaths:
             filepath = pathlib.Path(filepath) #to make sure that extracting the zip file gives the compressed files directly instead of giving them in a bunch of subdirectories
             archive.write(filepath, arcname=filepath.name)
+
+def extract_archive(filepaths, destination):
+    with zipfile.ZipFile(filepaths, 'r') as file:
+        file.extractall(destination)
+
--- main.py
+++ main.py
@@ -1,40 +1,63 @@
 import FreeSimpleGUI as sg
-import zipper
+import functions
+
+import time
 
 #define the labels first
-label1 = sg.Text("Select Files to Compress")
+label1 = sg.Text("Select File(s) to Compress or Extract")
 label2 = sg.Text("Select Destination Folder")
 
 #design the input box
-input_box1 = sg.InputText()
-input_box2 = sg.InputText()
+input_box1 = sg.InputText(expand_x=True)
+input_box2 = sg.InputText(expand_x=True)
 
 #design the buttons
 choose_button1 = sg.FilesBrowse("Choose", key='filepath')
 choose_button2 = sg.FolderBrowse("Choose", key='folderpath')
 compress_button = sg.Button("Compress")
+extract_button = sg.Button("Extract")
+exit_button = sg.Button("Exit")
 
 #define success message
-success_message = sg.Text(key="output")
+success_message1 = sg.Text(key="compressed")
+success_message2 = sg.Text(key="extracted")
 
 
 # design the gui window
-window = sg.Window(title="File Zipper",
+window = sg.Window(title="Ziptract",
                    layout=[[label1, input_box1, choose_button1],
                            [label2, input_box2, choose_button2],
-                           [compress_button, success_message]])
+                           [compress_button, extract_button, exit_button],
+                           [success_message1, success_message2]])
 
 while True:
     event, values = window.read()
-    print("event: ", event)
-    print("values: ", values)
-    filepaths = values['filepath'].split(";")
-    folderpaths = values['folderpath']
-    zipper.make_archive(filepaths, folderpaths)
-    window['output'].update(value = "Compression Successful!")
+    # print("event: ", event)
+    # print("values: ", values)
 
     match event:
-        case sg.WINDOW_CLOSED: break
+      case "Compress":
+        filepaths = values['filepath'].split(";")
+        folderpaths = values['folderpath']
+        functions.make_archive(filepaths, folderpaths)
+        window['compressed'].update(value = "Compression Successful!")
+        # make the success message disappear after 7 seconds
+        window.refresh()
+        time.sleep(7)
+        window['compressed'].update(value="")
+      case "Extract":
+          filepaths = values['filepath']
+          folderpaths = values['folderpath']
+          functions.extract_archive(filepaths, folderpaths)
+          window['extracted'].update(value="Extraction Successful!")
+          #make the success message disappear after 7 seconds
+          window.refresh()
+          time.sleep(7)
+          window['extracted'].update(value="")
+      case "Exit":
+          break
+
+
 
 window.close()
 
